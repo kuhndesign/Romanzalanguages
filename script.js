3e386dd5c1b0c07438f82201cc0699c7f0fd1d4f
kuhndesign <154625233+kuhndesign@users.noreply.github.com>
2024-02-08 17:10:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+//navbar
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+  navbar.classList.toggle('active');
+};
+
+//counter
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 5000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue +=1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
+
+
+
